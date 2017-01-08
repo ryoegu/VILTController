a8fcf5325c3c90df2d56489bb5129d8046a843cc
Ryo Eguchi <ryo@ryos-macbook-pro.local>
2017-01-08 17:24:05 +0900

    Initial Commit

---
+++ VILTController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VILTController
+//
+//  Created by Ryo Eguchi on 2017/01/08.
+//  Copyright © 2017年 Ryo Eguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
